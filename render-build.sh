f94c9b5c58d17713cbb76a970e9ffc03e09a3b49
Jerome-B-Nuwagaba <Nuwagababill5@gmail.com>
2026-04-27 13:30:02 +0300

    Add Composer install to build script

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
+
+# Install PHP dependencies
 composer install --no-dev --optimize-autoloader
+
+# Laravel setup
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
