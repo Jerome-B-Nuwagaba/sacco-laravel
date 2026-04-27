79292eafcbc6324ce492525fcb68c28584bbc43d
Jerome-B-Nuwagaba <Nuwagababill5@gmail.com>
2026-04-27 13:11:42 +0300

    Add Render deployment config

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+composer install --no-dev --optimize-autoloader
+php artisan config:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
